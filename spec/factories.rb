6badf5aa34f3a8fdabbde90061ce05435e9e7a1d
tsuyoshi <tsuyoshi.s85@gmail.com>
2014-05-04 04:05:30 +0900

    Add user microposts

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user do
-    sequence(:name) { |n| "Person #{n}" }
+    sequence(:name)  { |n| "Person #{n}" }
     sequence(:email) { |n| "person_#{n}@example.com" }
     password "foobar"
     password_confirmation "foobar"
@@ -8,5 +8,10 @@ FactoryGirl.define do
     factory :admin do
       admin true
     end
+  end
+
+  factory :micropost do
+    content "Lorem ipsum"
+    user
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,11 +15,10 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:remember_token) }
-
   it { should respond_to(:authenticate) }
+  it { should respond_to(:microposts) }
 
   it { should be_valid }
-  it { should respond_to(:authenticate) } 
 
   describe "when name is not present" do
     before { @user.name = " "}
@@ -132,4 +131,40 @@ describe User do
       end.to raise_error(ActiveModel::MassAssignmentSecurity::Error)
     end
   end
+
+  describe "micropost associations" do
+    before { @user.save }
+    let!(:older_micropost ) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.day.ago)
+    end
+
+    let!(:newer_micropost ) do
+      FactoryGirl.create(:micropost, user: @user, created_at: 1.hour.ago)
+    end
+
+    it "should have the right microposts in the right order" do
+      @user.microposts.should == [newer_micropost, older_micropost]
+    end
+
+    it "should destroy associated microposts" do
+      microposts = @user.microposts.dup
+      @user.destroy
+      microposts.should_not be_empty
+      microposts.each do |micropost|
+        Micropost.find_by_id(micropost.id).should be_nil
+      end
+    end
+
+    describe "status" do
+      let(:unfollowed_post) do
+        FactoryGirl.create(:micropost, user: FactoryGirl.create(:user))
+      end
+
+      its(:feed) { should include(newer_micropost) }
+      its(:feed) { should include(older_micropost) }
+      its(:feed) { should_not include(unfollowed_post) }
+
+    end
+  end
+
 end
